56d03fbbd641cbd2bfe0e7e3dca2efb8b27ac060
Roik, Vadym <vadym_roik_tp@bmc.com>
2018-04-16 13:54:11 +0300

    [new-feature] Adding Publisher entity

---
+++ src/main/java/com/example/gitdemo/model/Publisher.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Publisher {
+    private String firstName;
+    private String lastName;
+    private String nickName;
+}
